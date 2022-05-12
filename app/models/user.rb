e4c2e505133b6d0fd76e8f34f3eb49c924f87ac4
黑手（宋春月） <songchunyue@soyoung.com>
2022-05-12 10:40:36 +0800

    修改 ruby version

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,24 @@
 # Table name: `users`
 #
 # ### Columns
+#
 # Name                              | Type               | Attributes
 # --------------------------------- | ------------------ | ---------------------------
 # **`id`**                          | `bigint`           | `not null, primary key`
-# **`email`**
+# **`email`**                       | `string(100)`      |
+# **`nickname`**                    | `string(20)`       |
+# **`code`**                        | `string(6)`        |
+# **`password_digest`**             | `string(255)`      |
+# **`like_reports_count`**          | `integer`          | `default(0)`
+# **`like_votes_count`**            | `integer`          | `default(0)`
+# **`star_comments_count`**         | `integer`          | `default(0)`
+# **`followers_count`**             | `integer`          | `default(0)`
+# **`following_count`**             | `integer`          | `default(0)`
+# **`created_user_id(创建人 ID)`**  | `bigint`           |
+# **`updated_user_id(修改人 ID)`**  | `bigint`           |
+# **`disabled_at(删除时间)`**       | `datetime`         |
+# **`created_at`**                  | `datetime`         | `not null`
+# **`updated_at`**                  | `datetime`         | `not null`
 #
 class User < ApplicationRecord
   has_secure_password
@@ -16,10 +30,10 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
 
-  action_store :like, :report, user_counter_cache: true, counter_cache: true
-  action_store :like, :vote, user_counter_cache: true, counter_cache: true
-  action_store :star, :comment, user_counter_cache: true, counter_cache: true
-  action_store :follow, :user, counter_cache: 'followers_count', user_counter_cache: 'following_count'
+  # action_store :like, :report, user_counter_cache: true, counter_cache: true
+  # action_store :like, :vote, user_counter_cache: true, counter_cache: true
+  # action_store :star, :comment, user_counter_cache: true, counter_cache: true
+  # action_store :follow, :user, counter_cache: 'followers_count', user_counter_cache: 'following_count'
 
   def avatar_url
     avatar.attached? ? avatar.service_url : default_avatar_url
---
+++ db/schema.rb
@@ -0,0 +1,81 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_09_27_104406) do
+
+  create_table "actions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "action_type", null: false
+    t.string "action_option"
+    t.string "target_type"
+    t.integer "target_id"
+    t.string "user_type"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["action_type", "target_type", "target_id", "user_type", "user_id"], name: "uk_action_target_user", unique: true
+    t.index ["target_type", "target_id", "action_type"], name: "index_actions_on_target_type_and_target_id_and_action_type"
+    t.index ["user_type", "user_id", "action_type"], name: "index_actions_on_user_type_and_user_id_and_action_type"
+  end
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.bigint "created_user_id", comment: "创建人 ID"
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.bigint "created_user_id", comment: "创建人 ID"
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", limit: 100
+    t.string "nickname", limit: 20
+    t.string "code", limit: 6
+    t.string "password_digest"
+    t.integer "like_reports_count", default: 0
+    t.integer "like_votes_count", default: 0
+    t.integer "star_comments_count", default: 0
+    t.integer "followers_count", default: 0
+    t.integer "following_count", default: 0
+    t.bigint "created_user_id", comment: "创建人 ID"
+    t.bigint "updated_user_id", comment: "修改人 ID"
+    t.datetime "disabled_at", comment: "删除时间"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "versions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object", size: :long
+    t.datetime "created_at"
+    t.text "object_changes", size: :long
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+end
